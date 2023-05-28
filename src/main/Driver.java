2fbdd948ce2bb0fc0f288c1acafdec868b37ec5a
Yunus Emre Çiftçi <118928402+yunus-emre-ciftci@users.noreply.github.com>
2023-05-29 00:35:23 +0300

    Features of .start method explained

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -2,9 +2,24 @@ package main;
 
 import threadTest.MyThreadWithThread;
 
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
+        //Yeni bir MyThread objesi oluşturuldu.
         MyThreadWithThread myThreadWithThread = new MyThreadWithThread();
-        myThreadWithThread.run();
+        System.out.println("Start.");
+
+        //.run ile normal bir metot çağırmış gibi çağırım yaparız. Thread mantığıyla bir işlem yapılmaz. Sırayla yapılır.
+        //myThreadWithThread.run();
+
+        //.start metodu yeni bir thread objesi oluşturulur ve JVM referans objenin sınıfındaki run() metodunu çalıştırır.
+        // Alt satırlar bu aşamada .run() metodunun içindeki işlemlerin bitmesini beklemez. Çalışmaya başlarlar.
+        //.run içindeki işlemlerde asenkron şekilde çalışmaya başlar hangisi önce biterse ekrana o yazdırılır.
+        //.start metodunu iki kez çağıramazsınız. Birden fazla çalıştırmak isterseniz
+        // referans ettiği objenin sınıfını yeniden oluşturmamız gerekiyor. (myThreadWithThread = new MyThreadWithThread)
+        myThreadWithThread.start();
+
+        System.out.println("Finished.");
     }
 }
--- src/threadTest/MyThreadWithThread.java
+++ src/threadTest/MyThreadWithThread.java
@@ -1,8 +1,17 @@
 package threadTest;
 
+import java.util.Scanner;
+
 public class MyThreadWithThread extends Thread{
     @Override
     public void run() {
+        System.out.println("My thread is starting.");
+        try {
+            //.sleep için yazılan milisaniye tipinde bir long sayı girildiğinde o süre kadar işlem bekletilir.
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("My thread is running.");
     }
 }
