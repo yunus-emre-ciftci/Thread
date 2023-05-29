c939b2e4d65325835b5ced8809214ab92dc1bf2a
Yunus Emre Çiftçi <118928402+yunus-emre-ciftci@users.noreply.github.com>
2023-05-30 00:59:26 +0300

    MyThreadWithRunnable's run methods has been created.

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,11 +1,18 @@
 package main;
 
+import threadTest.MyThreadWithRunnable;
 import threadTest.MyThreadWithThread;
 
 import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
+        //test1();
+        test2();
+
+    }
+
+    public static void test1() {
         //Yeni bir MyThread objesi oluşturuldu.
         MyThreadWithThread myThreadWithThread = new MyThreadWithThread();
         System.out.println("Start.");
@@ -21,5 +28,17 @@ public class Driver {
         myThreadWithThread.start();
 
         System.out.println("Finished.");
+    }
+
+    public static void test2() {
+        //Runnable'ı implement eden kendi Thread sınıfımız doğru olan Thread kullanımıdır.
+        MyThreadWithRunnable myThreadWithRunnable = new MyThreadWithRunnable();
+        //myThreadWithRunnable doğrudan Thread sınıfının metotlarına erişemediği için bir Thread objesi oluşturuyoruz.
+        //parametre olarak Runnable objesi verebildiğimiz için myThreadWithRunnable objesini veriyoruz.
+        Thread thread = new Thread(myThreadWithRunnable);
+        System.out.println("My main is starting.");
+        //Şimdi Thread sınıfının metotlarına erişebiliriz.
+        thread.start();
+        System.out.println("My main is ending.");
     }
 }
--- src/threadTest/MyThreadWithRunnable.java
+++ src/threadTest/MyThreadWithRunnable.java
@@ -3,6 +3,13 @@ package threadTest;
 public class MyThreadWithRunnable implements Runnable{
     @Override
     public void run() {
-
+        System.out.println("My thread is starting.");
+        try {
+            //.sleep için yazılan milisaniye tipinde bir long sayı girildiğinde o süre kadar işlem bekletilir.
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("My thread is running.");
     }
 }
